0c4747d789e73cb1d268d9ddff03584f75c755de
DeadlyParkourKillerDarkStalker1988rus <kostabelgorod31@gmail.com>
2024-05-29 14:36:28 +0300

    16laba

---
+++ 2.c
@@ -0,0 +1,22 @@
+#include "libs/data_structures/matrix/matrix.h"
+
+int getMax(int *a, int n) {
+    int max = a[0];
+    for (int i = 1; i < n; i++)
+        if (a[i] > max)
+            max = a[i];
+    return max;
+}
+void sortRowsByMinElement(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
+}
+int main() {
+    int nRows, nCols;
+    scanf("%d %d", &nRows, &nCols);
+    matrix m = getMemMatrix(nRows, nCols);
+    inputMatrix(&m);
+    3
+    sortRowsByMinElement(m);
+    outputMatrix(m);
+    return 0;
+}
